5bf73448fb30591e05de770328e961ae575ca7b0
Jonatan Wincent <jonatan.wincent@proton.me>
2026-06-25 21:17:01 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,17 +13,25 @@
           path = ./go;
           description = "Go template";
         };
-        latex = {
-          path = ./latex;
-          description = "Latex template";
-        };
-        python = {
-          path = ./python;
-          description = "Python template";
+        go-package = {
+          path = ./go-package;
+          description = "Go package template";
         };
         jupyter = {
           path = ./jupyter;
           description = "Jupyter template";
+        };
+        latex = {
+          path = ./latex;
+          description = "Latex template";
+        };
+        pip = {
+          path = ./pip;
+          description = "Python pip template";
+        };
+        python = {
+          path = ./python;
+          description = "Python template";
         };
       };
     };
